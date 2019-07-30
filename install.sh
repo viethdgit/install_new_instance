96b8ee327d09eaf983a86a59e49dcd8a1532c918
viethdgit <uocthem1dieu@gmail.com>
2019-07-30 09:49:36 +0700

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-cd /opt/
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.2.0-linux-x86_64.tar.gz
-tar -xvzf elasticsearch-7.2.0-linux-x86_64.tar.gz
-mv elasticsearch-7.2.0 elasticsearch4
-rm -rf elasticsearch-7.2.0-linux-x86_64.tar.gz
